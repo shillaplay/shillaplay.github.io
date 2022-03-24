621c1c3a744d8fb092d2bc88e48b106c821d5c37
shillaplay <hello@shillainu.com>
2022-03-24 17:14:46 +0100

    CNAME fixed, cards & badge added, mobile sections image redesigned

---
+++ _next/static/chunks/836.bf60ca2035b486e3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[836],{3836:function(r,e,n){n.r(e);var t=n(85893),u=n(77044),a=n(81961),c=(0,u.AX)(a.AQ);e.default=function(r){var e=r.children,n=r.getLibrary;return(0,t.jsx)(c,{getLibrary:n,children:e})}}}]);
